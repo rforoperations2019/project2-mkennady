ee0be7e028bbbfa599909cab05381df3c8326e88
maxkennady <50968257+maxkennady@users.noreply.github.com>
2019-10-18 21:42:17 -0400

    added titles to graphs; created datatable
    added titles to graphs and map; created datatable; changed labels on bars and lines; removed data cleaning from app; edit text on validations; cleaned up previous code and comments; added comments and document code

--- app.R
+++ app.R
@@ -7,7 +7,6 @@ library(stringr)
 library(magrittr)
 library(plotly)
 library(ggplot2)
-#library(readr)
 library(DT)
 library(rgdal)
 library(leaflet)
@@ -16,47 +15,40 @@ library(datasets)
 library(lubridate)
 library(scales)
 
-GA.data <- read_csv("data/GAdata_week.csv")
-
-GA.data$X1 <- NULL
-GA.data$date <- gsub('^(.{4})(.*)$', '\\1-\\2', GA.data$date)
-GA.data$date <- gsub('^(.{7})(.*)$', '\\1-\\2', GA.data$date)
-GA.data$date <- ymd(GA.data$date)
-
-#GA.data2  <- read_csv("data/GAdata3.csv")
+GA.data <- read_csv("data/GAdata2018_2019.csv")
 
 cities <- read_csv("data/UScities.csv")
 
 state.names <- read_csv("data/state_names.csv")
 
-states <- geojson_read( 
-  x = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json", 
+states <- geojson_read(
+  x = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json",
   what = "sp")
-
 
 header <- dashboardHeader(title = "NAAEE Web Analytics")
 
-# Define UI for application
+# Define sidebar for dashboard
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Map", tabName = "map", icon = icon("map")),
     menuItem("Pageviews", tabName = "pageviews", icon = icon("chart-bar")),
     menuItem("Sources", tabName = "sources", icon = icon("arrows-alt-h")),
     menuItem("Pages", tabName = "pages", icon = icon("file")),
+    
     dateRangeInput("date", 
-                   label = "Date range",
-                   start = as.Date("2019-09-01"), end = as.Date("2019-09-08"),
-                   min = as.Date("2019-09-01"), max = as.Date("2019-09-08") ,
-                   format = "mm-dd-yyyy", startview = 'month'),
+                   label = "Select dates",
+                   start = as.Date("2019-09-01"), end = as.Date("2019-09-30"),
+                   format = "mm-dd-yyyy", startview = 'year'),
     textInput("text",
               label = "URL search",
               value = "naaee"),
     checkboxInput(inputId = "showCities",
                   label = "Show cities"),
     downloadButton(outputId = "download",
-                 label = "Download selected data"))
-)
+                 label = "Download selected data")
+))
 
+# create dashboard body
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "map",
@@ -66,18 +58,17 @@ body <- dashboardBody(
           ),
     tabItem(tabName = "pageviews",
             fluidRow(
-              
+              box(width = 10, plotlyOutput(outputId = "pageviewsLine"))
             )
     ),
     tabItem(tabName = "sources",
             fluidRow(
-              box(width = 9, plotlyOutput(outputId = "sourcesBar"))
-            )
-    ),
+              box(width = 10, plotlyOutput(outputId = "sourcesBar")))
+          ),
     tabItem(tabName = "pages",
             fluidRow(
               box(title = "Webpage Information Table", 
-                  DT::dataTableOutput(outputId = "datatable"), width = 6)
+                  DT::dataTableOutput(outputId = "datatable"), width = 12)
             )
           )
         )
@@ -88,42 +79,28 @@ ui <- dashboardPage(header, sidebar, body, skin = "green")
 # Define server
 server <- function(input, output) {
   
-  # observe({
-  #   if (str_detect(GA.data$pagePath, tolower(input$text), negate=FALSE) == TRUE) {
-  #     return(NULL)
-  #   } else {
-  #     return("naaee")
-  #   }
-  # })
-  
-  # input$text <- reactive({
-  #   if (str_detect(GA.data$pagePath, tolower(input$text), negate=FALSE) == TRUE) {
-  #     return(input$text)
-  #   } else {
-  #     return("naaee")
-  #   }
-  # })
-  
+  # get datasdet for inputs
   GAdata <- reactive({
-    #req(input$date, input$text)
+    req(input$date)
     
+    # ensure the text input is in the URLs
     shiny::validate(
-      need(str_detect(GA.data$pagePath, tolower(input$text), negate=FALSE) == TRUE , "Please type something in the text box")
+      need(str_detect(GA.data$pagePath, tolower(input$text), negate=FALSE) == TRUE , "Please type a valid URL in the text box")
     )
 
+    # ensure user hasn't entered blank test into the textbox
     shiny::validate(need(
-      input$text != "", "Please type something in the text box"
+      input$text != "", "Please type a valid URL in the text box"
     ))
     
+    # subset data
     subset(GA.data, GA.data$date >= input$date[1] &
              GA.data$date <= input$date[2] &
              (str_detect(GA.data$pagePath, tolower(input$text), negate=FALSE) == TRUE))
   })
   
+  # get dataset for cities map layer
   GAdata.pageviews <- reactive ({
-    # data.pageviews <- subset(GA.data, GA.data$date >= input$date[1] &
-    #                                   GA.data$date <= input$date[2] &
-    #                                   (str_detect(GA.data$pagePath, tolower(input$text), negate=FALSE) == TRUE))
     
     US.cities <- subset(GAdata(), country == "United States") %>%
       left_join(state.names, by = "region")
@@ -136,67 +113,66 @@ server <- function(input, output) {
     
     return(US.cities)
   })
-# Download button
   
-  # output$download <- downloadHandler(
-  #   filename = function() {
-  #     paste('NAAEE_WebsiteData.csv')
-  #   },
-  #   content = function(con) {
-  #     write_csv(GAdata(), con)
-  #   }
-  # )
+
+# Download csv of data
+  output$download <- downloadHandler(
+    filename = function() {
+      paste('NAAEE_WebsiteData.csv')
+    },
+    content = function(con) {
+      write_csv(GAdata(), con)
+    }
+  )
   
+  # create empty map
   output$USmap <- renderLeaflet({
-    
+
     geo.states <- states
+    
+    leaflet(geo.states) %>%
+      addProviderTiles("OpenStreetMap") %>%
+      setView(-98.5795, 39.8283, zoom = 4) %>%
+      addControl("<b>Pageviews and Users in the United States</b>", position = "topleft")
 
-    geo.states <- subset(geo.states, name != "Puerto Rico")
-    state.names <- unique(geo.states$name)
-
-    GAstate <- aggregate(pageviews ~ region, data = GAdata(), sum) %>%
-      subset(region %in% state.names)
-    colnames(GAstate) <- c("name", "pageviews")
-
-    geo.states@data <- left_join(geo.states@data, GAstate)
-
-    pal <- colorBin(palette = "Oranges",
-                    bins = 
-                      if(max(geo.states$pageviews, na.rm = TRUE) > 20) {
-                      c(0,
-                        round((max(geo.states$pageviews, na.rm = TRUE) + 1) / 5, 0),
-                        round((2 * max(geo.states$pageviews, na.rm = TRUE) + 1) / 5, 0),
-                        round((3 * max(geo.states$pageviews, na.rm = TRUE) + 1) / 5, 0),
-                        round((4 * max(geo.states$pageviews, na.rm = TRUE) + 1) / 5, 0),
-                        max(geo.states$pageviews, na.rm = TRUE) + 1)
-                    } else {
-                      c(0, 5, 10, 15, 20)
-                    },
-                    domain = geo.states$pageviews)
-
-    return(
-      leaflet(geo.states) %>%
-        addProviderTiles("OpenStreetMap") %>%
-        addPolygons(fillColor = ~pal(pageviews),
-                    weight = 1,
-                    opacity = 1,
-                    color = "black",
-                    fillOpacity = 0.75,
-                    popup = ~paste0("<b>", name, "</b> ",
-                                    "<br>Pageviews: ",
-                                      format(pageviews, big.mark = "," ))) %>%
-        addLegend(position = "bottomright",
-                  pal = pal,
-                  values = ~pageviews,
-                  title = "Pageviews") %>%
-        mapOptions(zoomToLimits = "first")
-    )
     })
   
-  
-  
+  # choropleth map of US states showing pageviews
   observe({
-    #necessary? req(input$date, input$text)
+    
+    geo.states <- states
+    USmap.data <- GAdata()
+    
+    geo.states <- subset(geo.states, name != "Puerto Rico")
+    state.names <- unique(geo.states$name)
+    
+    USmap.data <- aggregate(list(USmap.data$pageviews, USmap.data$users), by = list(USmap.data$region), sum) 
+    colnames(USmap.data) <- c("name", "pageviews", "users")
+    subset(USmap.data, name %in% state.names)
+    
+    geo.states@data <- left_join(geo.states@data, USmap.data)
+    
+    pal <- colorNumeric(palette = "YlOrBr",
+                        alpha = .2, 
+                        domain = c(0, max(geo.states$pageviews)))
+    
+    leafletProxy("USmap", data = geo.states) %>%
+      clearGroup("polygon") %>%
+      addPolygons(fillColor = ~pal(pageviews),
+                  weight = 1,
+                  opacity = 1,
+                  color = "black",
+                  fillOpacity = 0.75,
+                  group = "polygon",
+                  popup = ~paste0("<b>", name, "</b> ",
+                                  "<br>Pageviews: ",
+                                  format(pageviews, big.mark = "," ),
+                                  "<br>Users: ",
+                                  format(users, big.mark = "," )))
+    })
+  
+  # add layer of cities to map
+  observe({
     
     city.data <- GAdata.pageviews()
 
@@ -205,17 +181,18 @@ server <- function(input, output) {
         addMarkers(lng = ~lng,
                    lat = ~lat,
                    popup = ~paste0("<b>", city.state, "</b> ",
-                                   "<br><b>", format(input$date[1], "%m/%d/%Y"), " - ", format(input$date[2], "%m/%d/%Y"), "</b>",
                                    "<br>Pageviews: ", format(pageviews, digits = 0, big.mark = "," ),
                                    "<br>Users: ", format(users, digits = 0, big.mark = "," )))  %>%
         mapOptions(zoomToLimits = "first")
     } else {
       leafletProxy("USmap", data = city.data) %>%
-        clearMarkers() %>%
-        mapOptions(zoomToLimits = "first")
+        clearMarkers()
     }
   })
   
+  # Create bar graph of sources data
+  # Reference:
+  # https://stackoverflow.com/questions/34605919/formatting-mouse-over-labels-in-plotly-when-using-ggplotly
   output$sourcesBar <- renderPlotly({
     
     bar.data <- GAdata()
@@ -225,47 +202,87 @@ server <- function(input, output) {
     
     # Top 10 sources by number of users
     bar.data <- top_n(bar.data, 10, users)
+    
     ggplotly(
-      ggplot(data=bar.data, aes(x=reorder(source, users), y=users)) +
-        geom_bar(stat="identity", fill="olivedrab3") +
+      ggplot(data=bar.data, aes(x=reorder(source, users), y=users, group=1)) +
+        geom_bar(stat="identity", fill="#74A542", aes(text = paste0("<b>Number of users: </b>", 
+                                                                    format(users, big.mark = "," )))) +
         coord_flip() +
         labs(x ="Source", 
              y = "Number of Users") + 
-        theme(axis.title = element_text(color="deepskyblue4", size=16, face="bold"),
+        theme(axis.title = element_text(color="#027E98", size=16, face="bold"),
               axis.text = element_text(face = "bold", size = 12),
               axis.text.x = element_text(angle = 15),
               axis.ticks = element_blank(),
               panel.background = element_rect(fill = "white"),
               panel.grid.major.x = element_line(colour = "gray60"),
               panel.grid.major.y = element_line(colour = "white"),
-              panel.grid.minor = element_line(colour = "white", 
-                                              linetype = "dashed")) + 
+              panel.grid.minor = element_line(colour = "white")) + 
         scale_y_continuous(labels = comma, 
-                           breaks = seq(0, max(bar.data$users), signif(max(bar.data$users), digits = 1) / 10))
-    )
-
+                           breaks = seq(0, max(bar.data$users), signif(max(bar.data$users), digits = 1) / 10)) +
+        ggtitle(paste0("Top sources for URLs containing \"", input$text, "\"")),
+      tooltip = "text")
   })
-
-  output$datatable <-
-    DT::renderDataTable({
-      DT::datatable(data = GAdata())
+  
+  # create line graph of pageviews
+  output$pageviewsLine <- renderPlotly({
+    
+    line.data <- GAdata()
+    
+    line.data <- aggregate(list(line.data$pageviews, line.data$users), by = list(line.data$date), sum)
+    
+    colnames(line.data) <- c("date", "pageviews" ,"users")
+  
+    ggplotly(
+      ggplot(data = line.data, aes(x = date, y = pageviews, group=1))+
+        geom_line(color = "#74A542", size = 2, aes(text = paste0("<b>Date: </b>", format(date, "%m/%d/%Y"), 
+                                                                 "<br><b>Number of pageviews: </b>", format(pageviews, digits = 0, big.mark = ",")))) +
+        labs(x ="Date", 
+             y = "Pageviews") + 
+        theme(axis.title = element_text(color="#027E98", size=15, face="bold"),
+              axis.text = element_text(size = 12, face = "bold"),
+              axis.ticks = element_blank(),
+              panel.background = element_rect(fill = "white"),
+              panel.grid.major = element_line(colour = "gray60"),
+              panel.grid.minor = element_line(colour = "white", 
+                                              linetype = "dashed")) +
+        scale_y_continuous(labels = comma) +
+        ggtitle(paste0("Pageviews for URLs containing \"", input$text, "\"")),
+      tooltip = "text"
+    )
     })
-  
-  
-  
-  # output$datatable <- 
-  #   DT::renderDataTable({
-  #     DT::datatable(data = GA.data(), options = list(
-  #       autoWidth = TRUE,
-  #       scrollX = TRUE,
-  #       scrollY = 400
-  #     ), colnames = c("Date", "URL", "Page Title", "Users", "Pageviews", "Sessions", "Bounce Rate", "Exit Rate", "Avg Time on Page", "Entrances"))
-  #   })
-  
 
-  
-  
-  
+  # create datatable of selected dataset
+  output$datatable <-
+    
+    DT::renderDataTable({
+      
+      DTdata <- GAdata()
+      
+      DTdata$city <- NULL
+      DTdata$entrances <- NULL
+      
+      DTdata<- DTdata[,c(1,2,6:11,3:5)]
+      
+      colnames(DTdata) <- c("Date","Page Path", "Users", "Pageviews","Sessions","Bounce Rate (%)",
+                            "Exit Rate (%)","Avg Time on Page (sec)","Source","Region","Country")
+      
+      DTdata$`Avg Time on Page (sec)` <- format(DTdata$`Avg Time on Page (sec)`, digits = 0)
+      DTdata$`Bounce Rate (%)` <- format(DTdata$`Bounce Rate (%)`, digits = 2)
+      DTdata$`Exit Rate (%)` <- format(DTdata$`Exit Rate (%)`, digits = 2)
+      
+      DT::datatable(data = DTdata, 
+                    class = 'cell-border stripe', 
+                    rownames = FALSE,
+                    filter = "top",
+                    options = list(
+                      order = list(list(0, 'asc')),
+                      autoWidth = TRUE,
+                      scrollX = TRUE,
+                      scrollY = 400
+                      # dom = 't'
+      ))
+    })
 }
 
 # Run the application 
